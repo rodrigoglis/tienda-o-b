d12d74e13bbe9e76a9f6a4b42ec67f2ccc653da4
rodrigoglis <rodrigoglisciak@hotmail.com>
2020-11-28 16:58:46 -0300

    mod de nav

--- script/boton.js
+++ script/boton.js
@@ -1,24 +1,7 @@
+function navb() {
 
-addEventListener("DOMContentLoaded",()=> {
+    let nav = document.getElementById("nav");
+    nav.classList.toggle("active");
 
-
-    const btn_menu =document.querySelector(".btn")
-
-    if (btn_menu) {
-        
-
-        btn_menu.addEventListener("click",() =>{
-
-            btn_iten = document.querySelector("nav")
-
-            btn_iten.classList.toggle("windows")
-
-
-        })
-    }
-
-
-
-
-
-})
+    
+}
